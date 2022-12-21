e842df614566b7349658bdda48bae383c38325df
gwt805 <1973735972@qq.com>
2022-12-21 20:52:15 +0800

    update

--- myess/ess_auto_start.sh
+++ myess/ess_auto_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 str=$"\n"
-cd /home/root/myess
+cd /home/weitao/myess
 nohup /home/${USER}/anaconda3/bin/python myess/manage.py runserver 0.0.0.0:8088 > /home/${USER}/myess/ESS.log 2>&1 &
 sstr=$(echo -e $str)
 echo $sstr
