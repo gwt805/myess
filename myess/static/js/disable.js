23180687ad3718601e83d7b13dc318e0054748c7
guoweitao <guoweitao@gs-robot.com>
2022-12-23 12:53:39 +0800

    add about of ess info and update show developtools method

--- myess/static/js/disable.js
+++ myess/static/js/disable.js
@@ -18,14 +18,26 @@ document.oncontextmenu = function () {
     return false;
 }
 
-// 监视窗口大小
-var h = window.innerHeight
-var w = window.innerWidth;
-window.onresize = function () {
-    if (h != window.innerHeight || w != window.innerWidth) {
-        window.location = "https://www.baidu.com";
+// 禁止打开开发者工具
+var threshold = 160; // 打开控制台的宽或高阈值
+// 每秒检查一次
+setInterval(function () {
+    if (window.outerWidth - window.innerWidth > threshold ||
+        window.outerHeight - window.innerHeight > threshold) {
+        // 如果打开控制台，关闭页面
+        if (navigator.userAgent.indexOf('MSIE') > 0) { // close IE
+            if (navigator.userAgent.indexOf('MSIE 6.0') > 0) {
+                window.location.href = "https://www.baidu.com/";
+            } else {
+                window.location.href = "https://www.baidu.com/";
+            }
+        } else { // close chrome;It is effective when it is only one.
+            window.location.href = "https://www.baidu.com/";
+        }
     }
-};
+}, 1000);
+
+// check();
 // 禁止选取内容
 // document.onselectstart = function () {
 //     return false;
