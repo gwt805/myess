4d774bf98f4dd4e8d812fef351117ba3a6219de9
gwt805 <1973735972@qq.com>
2023-02-13 14:12:21 +0800

    fix bug

--- myess/cronjob/views.py
+++ myess/cronjob/views.py
@@ -201,8 +201,8 @@ def ding_day_report_form():
 
 
 def every_day_ding_send_report_form():
-    _, char_list, money_total, _ = views.wbdata_count_public_code("是", "---", "", "")
-    make_report_form_img(char_list, money_total)
+    _, _, char_list, money_total, _ = views.wbdata_count_public_code("是", "---", "", "")
+    make_report_form_img(char_list, format(round(money_total, 2), ','))
     ding_day_report_form()
     
 
